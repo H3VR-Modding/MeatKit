ca4a00524ccee317d0f1d903fef5c26afe938370
Nathan Gill <nrgill28@gmail.com>
2021-11-21 18:43:35 -0500

    Small tweaks

--- Assets/MeatKit/Editor/Extensions.cs
+++ Assets/MeatKit/Editor/Extensions.cs
@@ -79,18 +79,14 @@ namespace MeatKit
         public static Texture2D ScaleTexture(this Texture2D source, int targetWidth, int targetHeight)
         {
             Texture2D result = new Texture2D(targetWidth, targetHeight, TextureFormat.ARGB32, false);
-            float incX = 1.0f / targetWidth;
-            float incY = 1.0f / targetHeight;
             for (int i = 0; i < result.height; ++i)
             {
                 for (int j = 0; j < result.width; ++j)
                 {
-                    Color newColor = source.GetPixelBilinear(j / (float) result.width,
-                        i / (float) result.height);
+                    Color newColor = source.GetPixelBilinear(j / (float) result.width, i / (float) result.height);
                     result.SetPixel(j, i, newColor);
                 }
             }
-
             result.Apply();
             return result;
         }
