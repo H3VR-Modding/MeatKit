b9b6770ed53ee038a884d45261ac0abbc0cee1ee
Nathan Gill <nrgill28@gmail.com>
2022-02-27 12:44:26 -0500

    Added some new icons, extra guards in prefab loader.

--- Assets/MeatKit/Tools/Editor/PrefabLoader.cs
+++ Assets/MeatKit/Tools/Editor/PrefabLoader.cs
@@ -23,15 +23,32 @@ public class PrefabLoader : EditorWindow
 
             // Ask for the new bundle, load it, and get its assets
             string assetBundlePath = EditorUtility.OpenFilePanel("Select Asset Bundle", string.Empty, string.Empty);
-            _bundle = AssetBundle.LoadFromFile(assetBundlePath);
-            _assets = _bundle.GetAllAssetNames();
-            _selectedAsset = 0;
+            
+            // Make sure the user actually selected a file
+            if (!string.IsNullOrEmpty(assetBundlePath))
+            {
+                _bundle = AssetBundle.LoadFromFile(assetBundlePath);
+                
+                // Make sure a valid bundle was selected
+                if (_bundle != null)
+                {
+                    _assets = _bundle.GetAllAssetNames();
+                    _selectedAsset = 0;
+                }
+            }
         }
 
+        // Only show spawn button if there's at least one asset
         if (_assets.Length > 0)
         {
             _selectedAsset = EditorGUILayout.Popup(_selectedAsset, _assets);
             if (GUILayout.Button("Spawn")) Instantiate(_bundle.LoadAsset(_assets[_selectedAsset]));
+            
+            // Warn the user about the play mode thing
+            if (!EditorApplication.isPlaying)
+            {
+                EditorGUILayout.HelpBox("References on a prefab loaded object will break after a restart of the editor unless you enter play mode first.", MessageType.Warning);
+            }
         }
     }
 }
