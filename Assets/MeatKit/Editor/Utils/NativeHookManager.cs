69e1c7ced6185ee7dfb121a5205e4688b4434d02
Nathan Gill <nrgill28@gmail.com>
2022-06-20 13:24:03 -0400

    Inform the user if they're using the incorrect version of Unity

--- Assets/MeatKit/Editor/Utils/NativeHookManager.cs
+++ Assets/MeatKit/Editor/Utils/NativeHookManager.cs
@@ -4,6 +4,7 @@ using System.Runtime.InteropServices;
 using MonoMod.RuntimeDetour;
 using MonoMod.Utils;
 using UnityEditor;
+using UnityEngine;
 
 namespace MeatKit
 {
@@ -23,23 +24,37 @@ namespace MeatKit
         // Keep track of all the applied detours so we can quickly undo them before the mono domain is reloaded
         private static readonly List<NativeDetour> Detours = new List<NativeDetour>();
 
+        private static readonly bool AreWeCorrectUnityVersion = false;
+
         static NativeHookManager()
         {
+            AreWeCorrectUnityVersion = Application.unityVersion == "5.6.3p4";
+            if (!AreWeCorrectUnityVersion)
+            {
+                EditorUtility.DisplayDialog("Whoops",
+                    "Looks like you aren't using Unity Editor 5.6.3p4, MeatKit v2 now requires this specific editor version.",
+                    "I'll go install that.");
+                return;
+            }
+
             // Apply our detours here and save the trampoline to call the original function
             OrigShutdownManaged = ApplyEditorDetour<ShutdownManaged>(0x17542D0, new ShutdownManaged(OnShutdownManaged));
         }
 
         public static T ApplyEditorDetour<T>(long from, Delegate to) where T : class
         {
+            // Avoid crashing the editor if we're loaded in the wrong Unity version
+            if (!AreWeCorrectUnityVersion) return null;
+
             // Get the base address of the Unity module and the address in memory of the function
             IntPtr editorBase = DynDll.OpenLibrary("Unity.exe");
-            IntPtr fromPtr = (IntPtr) (editorBase.ToInt64() + from);
+            IntPtr fromPtr = (IntPtr)(editorBase.ToInt64() + from);
 
             // Get a function pointer for the managed callback
             var toPtr = Marshal.GetFunctionPointerForDelegate(to);
 
             // Make a detour and add it to the list
-            var detour = new NativeDetour(fromPtr, toPtr, new NativeDetourConfig {ManualApply = true});
+            var detour = new NativeDetour(fromPtr, toPtr, new NativeDetourConfig { ManualApply = true });
             Detours.Add(detour);
 
             // Apply the detour and generate a trampoline for it, which we return
@@ -50,11 +65,14 @@ namespace MeatKit
 
         public static Delegate GetDelegateForFunctionPointer<T>(long from)
         {
+            // Avoid crashing the editor if we're loaded in the wrong Unity version
+            if (!AreWeCorrectUnityVersion) return null;
+
             // Get the base address for the Unity module and apply the offset
             IntPtr editorBase = DynDll.OpenLibrary("Unity.exe");
-            return Marshal.GetDelegateForFunctionPointer((IntPtr) (editorBase.ToInt64() + from), typeof(T));
+            return Marshal.GetDelegateForFunctionPointer((IntPtr)(editorBase.ToInt64() + from), typeof(T));
         }
-        
+
         private static void OnShutdownManaged()
         {
             // Unity is about to shutdown the mono runtime! Quickly dispose of our detours!
