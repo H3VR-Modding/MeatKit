85168778378d69df4d42feceb82b88d965eaf787
Nathan Gill <nrgill28@gmail.com>
2023-09-23 00:05:44 -0400

    Force load our newer Mono.Cecil to prevent a crash

---
+++ Assets/MeatKit/Editor/Utils/AssemblyResolverFix.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace MeatKit
+{
+    [InitializeOnLoad]
+    public static class AssemblyResolverFix
+    {
+        static AssemblyResolverFix()
+        {
+            // This is required because Unity ships its own Mono.Cecil with the editor that is older
+            // If Unity's older one is loaded first it will not automatically load our newer one, and that is going to cause problems
+            // So here we _also_ load the newer one. Yes, there will be two of them loaded. That should be OK since Unity doesn't expose any of its
+            // Mono.Cecil to user code so we should never be interacting with it.
+            string asmPath = Path.Combine(Application.dataPath, "Managed/Mono.Cecil.dll");
+            Assembly.LoadFile(asmPath);
+        }
+    }
+}
